3ea56382b8265595e30606c641544af2720c7203
許皇琪 <a3547@gding.com.tw>
2023-06-30 16:26:36 +0800

    feat: redis address changed

--- models/redis.js
+++ models/redis.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis'
 
 export const redisCreate = () => {
-  const client = createClient({ url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}` })
+  const client = createClient({ url: 'redis://redis:6379' })
   // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
     client.on('error', (err) => reject(err))
